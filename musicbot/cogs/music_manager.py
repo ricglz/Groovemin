064c9166d3088dc2a16e590fb0e8c66cddaa32df
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2021-09-14 17:06:21 -0500

    Add skip command

--- musicbot/cogs/music_manager.py
+++ musicbot/cogs/music_manager.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from math import ceil
 from typing import Optional
 import logging
 
@@ -108,7 +109,7 @@ class MusicManager(Cog):
                 "Removed entry `{0}`"
             ).format(entry.title)
         response_msg = response_msg.strip()
-        self.safe_send_message(context, response_msg)
+        await self.safe_send_message(context, response_msg)
 
     @command
     async def queue(self, context: Context):
@@ -176,4 +177,76 @@ class MusicManager(Cog):
             )
 
         message = '\n'.join(lines)
-        self.safe_send_message(context, message)
+        await self.safe_send_message(context, message)
+
+    async def cmd_skip(self, context: Context):
+        player = self._get_player(context.channel)
+        if player.is_stopped:
+            error_msg = self.str.get('cmd-skip-none', "Can't skip! The player is not playing!")
+            raise CommandError(error_msg, expire_in=20)
+
+        if not player.current_entry:
+            if player.playlist.peek():
+                if player.playlist.peek()._is_downloading:
+                    response_msg = self.str.get(
+                        'cmd-skip-dl',
+                        "The next song (`%s`) is downloading, please wait."
+                    ) % player.playlist.peek().title
+                    return await self.safe_send_message(context, response_msg)
+
+                elif player.playlist.peek().is_downloaded:
+                    print("The next song will be played shortly. Please wait.")
+                else:
+                    print("Something odd is happening.  "
+                          "You might want to restart the bot if it doesn't start working.")
+            else:
+                print("Something strange is happening.  "
+                      "You might want to restart the bot if it doesn't start working.")
+
+        current_entry = player.current_entry
+
+        voice_channel = context.guild.me.voice.channel
+        num_voice = sum(
+            1 for m in voice_channel.members
+              if not (m.voice.deaf or m.voice.self_deaf or m == self.bot.user)
+        )
+
+        # Incase all users are deafened, to avoid division by zero
+        if num_voice == 0:
+            num_voice = 1
+
+        num_skips = player.skip_state.add_skipper(context.author.id, context.message)
+
+        skips_remaining = min(
+            self.config.skips_required,
+            ceil(self.config.skip_ratio_required / (1 / num_voice))
+        ) - num_skips
+
+        if skips_remaining <= 0:
+            # check autopause stuff here
+            player.skip()
+            extra_msg = self.str.get(
+                'cmd-skip-reply-skipped-2',
+                ' Next song coming up!'
+            ) if player.playlist.peek() else ''
+            response_msg = self.str.get(
+                'cmd-skip-reply-skipped-1',
+                'Your skip for `{0}` was acknowledged.\nThe vote to skip has been passed.{1}'
+            ).format(current_entry.title, extra_msg)
+
+        else:
+            # TODO: When a song gets skipped, delete the old x needed to skip
+            # messages
+            extra_msg = self.str.get('cmd-skip-reply-voted-2', 'person is') \
+                        if skips_remaining == 1 \
+                        else self.str.get('cmd-skip-reply-voted-3', 'people are')
+            response_msg = self.str.get(
+                'cmd-skip-reply-voted-1',
+                'Your skip for `{0}` was acknowledged.\n**{1}** more {2} required to vote to skip this song.'
+            ).format(
+                current_entry.title,
+                skips_remaining,
+                extra_msg
+            )
+
+        await self.safe_send_message(context, response_msg)
