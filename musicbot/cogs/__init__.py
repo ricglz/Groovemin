d372c38519e9c5a1ff236870114be0ba085f7aba
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2021-09-15 12:54:41 -0500

    Add new cogs to the list to be added by the bot

--- musicbot/cogs/__init__.py
+++ musicbot/cogs/__init__.py
@@ -1,16 +1,19 @@
 '''Module containing all the Cogs for music bot'''
 from typing import List
 
+from .connection_manager import ConnectionManagerCog
 from .custom_cog import CustomCog as Cog
 from .messenger import MessengerCog
-from .music_manager import MusicManager
+from .music_manager import MusicManagerCog
+from .now_playing import NowPlayingCog
 from .play import PlayCog
 from .player import PlayerCog
-from .connection_manager import ConnectionManagerCog
 
 COGS: List[Cog] = [
     ConnectionManagerCog,
     MessengerCog,
+    MusicManagerCog,
+    NowPlayingCog,
     PlayCog,
     PlayerCog,
 ]
