249d39c57614955a65bfa9d316849113303b7256
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2021-09-16 17:38:04 -0500

    Fix linting issues in messenger

--- musicbot/cogs/messenger.py
+++ musicbot/cogs/messenger.py
@@ -1,28 +1,38 @@
+'''Module containing messenger cog'''
 import asyncio
 import logging
+from typing import Optional, Union
 
 import discord
+from discord import Embed, TextChannel
+from discord.ext.commands import Context
+from dislash import SlashInteraction
 
 from ..constants import DISCORD_MSG_CHAR_LIMIT
 from .custom_cog import CustomCog
 
 log = logging.getLogger(__name__)
 
+Content = Union[str, Embed]
+Destination = Union[Context, SlashInteraction, TextChannel]
+
 class MessengerCog(CustomCog):
-    async def safe_send_message(self, dest, content, **kwargs):
-        tts = kwargs.pop('tts', False)
-        quiet = kwargs.pop('quiet', False)
-        expire_in = kwargs.pop('expire_in', None)
-        allow_none = kwargs.pop('allow_none', True)
+    '''Cog class in charge of sending and removing messages'''
+    async def safe_send_message(self, dest: Destination, content: Content, **kwargs):
+        '''Send messages to the specified destination'''
+        tts: bool = kwargs.pop('tts', False)
+        quiet: bool = kwargs.pop('quiet', False)
+        expire_in: Optional[float] = kwargs.pop('expire_in', None)
+        allow_none: bool = kwargs.pop('allow_none', True)
 
         msg = None
         log_func = log.debug if quiet else log.warning
 
         try:
             if content is not None or allow_none:
-                if isinstance(dest, discord.TextChannel):
+                if isinstance(dest, TextChannel):
                     msg = await dest.send(content, tts=tts, delete_after=expire_in)
-                elif isinstance(content, discord.Embed):
+                elif isinstance(content, Embed):
                     msg = await dest.reply(embed=content, delete_after=expire_in)
                 else:
                     msg = await dest.reply(content, tts=tts, delete_after=expire_in)
@@ -47,6 +57,7 @@ class MessengerCog(CustomCog):
         await self.safe_delete_message(message, quiet=True)
 
     async def safe_delete_message(self, message, *, quiet=False):
+        '''Deletes a sent message'''
         log_func = log.debug if quiet else log.warning
 
         try:
