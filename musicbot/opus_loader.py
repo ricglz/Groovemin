139853c56769cd5d4fbcc0787e0584105ced5c5c
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2021-09-26 11:28:05 -0500

    Fix some lint issues in opus loader

--- musicbot/opus_loader.py
+++ musicbot/opus_loader.py
@@ -1,13 +1,14 @@
+'''Module containing a single function to load the opus library'''
 from discord import opus
 
 def load_opus_lib():
+    """Loads discord's opus library"""
     if opus.is_loaded():
         return
-
     try:
         opus._load_default()
         return
     except OSError:
-         pass
+        pass
 
     raise RuntimeError('Could not load an opus lib.')
